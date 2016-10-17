7f61e70c5b1a346ca3806fa74d9a3fa6d6f6b1a0
Sinethemba <dSinethemba@gmail.com>
2016-10-17 11:05:48 +0200

    functioned objects on JS

--- findItemsOver.js
+++ findItemsOver.js
@@ -4,14 +4,13 @@ var findItemsOver = function(items,threshold)
 {
     var over20 = [];
 
-    for(var i = 0; i <items.length; i++)
+    for(var i = 0; i < items.length; i++)
     {
-      if(items.price > threshold)
+      if(items[i].price > threshold)
       {
         over20.push(items[i]);
       }
     }
-
     return over20;
 }
 
@@ -21,5 +20,14 @@ var itemList = [
     {name : 'bananas', price : 27},
     {name : 'apples', price : 3}];
 
+var itemList2 = [
+        {name : 'coffee', price : 25},
+        {name : 'tea', price : 15},
+        {name : 'sandwich', price : 25},
+        {name : 'muffin', price : 10}];
 
-assert.deepEqual(findItemsOver(itemList,20), [ { name: 'pears', price: 37 }, { name: 'bananas', price: 27 } ] );
+var storeFunc = findItemsOver(itemList,20);
+var storeFunc2 = findItemsOver(itemList2,20);
+
+assert.deepEqual(storeFunc, [ { name: 'pears', price: 37 }, { name: 'bananas', price: 27 } ] );
+assert.deepEqual(storeFunc2, [ { name: 'coffee', price: 25 }, { name: 'sandwich', price: 25 } ] );
