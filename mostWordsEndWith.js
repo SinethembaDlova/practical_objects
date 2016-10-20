ca2429d63851cb1f0d99352f5926ed45bc05ab6a
Sinethemba <dSinethemba@gmail.com>
2016-10-20 16:31:19 +0200

    modified

--- mostWordsEndWith.js
+++ mostWordsEndWith.js
@@ -2,10 +2,12 @@ const assert = require('assert');
 
 var mostWordsEndWith = function(commonEnd)
 {
-  var splittedWords = commonEnd.split(' ');
+  var rmChar = commonEnd.replace(','&&'.', "" );
+  var splittedWords = rmChar.split(' ');
   var letterMap = {};
   var highestNum = 0;
   var dominantLetter;
+  var arr = [];
 
   for( var i = 0; i < splittedWords.length; i++)
   {
@@ -17,32 +19,39 @@ var mostWordsEndWith = function(commonEnd)
     }
 
     letterMap[storeLetters]++;
-  }
+
     //console.log(letterMap);
 
   for(var numLetter in letterMap)
   {
+
     if(highestNum < letterMap[numLetter])
     {
       highestNum = letterMap[numLetter];
       dominantLetter = numLetter;
     }
-
-    if (splittedWords[i].endsWith(dominantLetter))
-    {
-     console.log(splittedWords[i]);
-    }
   }
+ }
+  arr.push(
+   {
+        highVal  : highestNum ,
+        letter: dominantLetter
+   });
+  //  console.log(map.letter + '======================= CheckwithLetter');
+  //  console.log(splittedWords);
 
 
+    if (splittedWords.endsWith(dominantLetter))
+    {
+     console.log(splittedWords);
+    }
 
 
-
-  return {highestNum, dominantLetter};
+  return arr;
     //console.log(storeLetters);
 
 }
 
-var sentence = 'Down by the river there is a man that quiver and shiver , but he needs to deliver a packet that he think is a big racket and a packet of gum .';
+var sentence = 'Down by the river there is a man that quiver and shiver, but he needs to deliver a packet that he think is a big racket and a packet of gum.';
 mostWordsEndWith(sentence);
 console.log(mostWordsEndWith(sentence));
